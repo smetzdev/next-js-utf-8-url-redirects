c03ef6e37ca611200188e0fa6a1262d23a85e45b
Sascha Metz <hello@smetz.dev>
2024-04-10 16:29:17 +0200

    add url redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        // süper should redirect to sueper (with static url encoded string)
+        source: "/s%C3%BCper",
+        destination: "/sueper",
+        permanent: true,
+      },
+      {
+        // düper should redirect to sueper (with a node function)
+        source: encodeURI("/düper"),
+        destination: "/sueper",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
